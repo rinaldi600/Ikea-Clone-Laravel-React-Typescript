5f9861af1ae0a0848791e79f065e78db12f83edf
rinaldi600 <rinaldih84@gmail.com>
2023-10-18 22:37:59 +0700

    Build banner home 2

---
+++ resources/js/Pages/Home/Banner/Banner.tsx
@@ -0,0 +1,16 @@
+function Banner() {
+    return (
+        <>
+            <div className="h-[369.013px] bg-red-400 w-[100%] xl:w-[95%]">
+
+            </div>
+            <div className="flex pt-3 items-center">
+                <div className="w-[426.358px] bg-[#F5F5F5] h-[5px] rounded-l"></div>
+                <div className="w-[426.358px] bg-[#F5F5F5] h-[5px]"></div>
+                <div className="w-[426.358px] bg-[#F5F5F5] h-[5px] rounded-r"></div>
+            </div>
+        </>
+    )
+}
+
+export default Banner;
--- resources/js/Pages/Home/Home.tsx
+++ resources/js/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../Layout/Layout";
 import { Link } from "@inertiajs/react";
+import Banner from "./Banner/Banner";
 
 function Home() {
     return (
@@ -13,9 +14,15 @@ function Home() {
                     <Link href="/">Solusi kurangi polusi</Link>
                     <Link href="/">Rumahmu, perlengkapanmu, caramu!</Link>
                 </div>
-                <div className="xl:pl-32">
-                    <div className="min-h-[50px] w-[100%] xl:w-[95%] bg-blue-300">
-                        <h1>WORK</h1>
+                <div className="xl:pl-32 pt-20">
+                    <div className="min-h-[50px] rounded flex items-center gap-6 p-2 bg-[#EFEFEF] w-[100%] xl:w-[95%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[24px] text-[#484848] fill-[#484848] h-[24px]">
+                        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+                        </svg>
+                        <Link className="text-sm hover:no-underline underline decoration-1 text-[#121416]" href="/">Selalu pastikan iklan promosi IKEA Indonesia yang valid, berasal dari website IKEA Indonesia. Klik untuk detailnya</Link>
+                    </div>
+                    <div className="pt-9">
+                        <Banner/>
                     </div>
                 </div>
             </div>
