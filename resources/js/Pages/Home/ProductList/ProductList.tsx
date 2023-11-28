a5b680da13df232fb41a418fef1cb62d2b5d4131
rinaldi600 <rinaldih84@gmail.com>
2023-11-28 22:49:45 +0700

    Build product list 3

--- resources/js/Pages/Home/ProductList/ProductList.tsx
+++ resources/js/Pages/Home/ProductList/ProductList.tsx
@@ -2,24 +2,24 @@ function ProductList() {
     return (
         <div className="pt-7">
             <h1 className="text-3xl pb-5 font-bold">Produk Pilihan</h1>
-            <div className="min-h-[466.175px] grid grid-flow-col w-[100%] xl:w-[95%] bg-red-400">
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px] md:w-[277px] lg:w-[259px] w-[268px] bg-yellow-400">
-
+            <div className="min-h-[466.175px] xl:justify-around overflow-y-hidden xl:gap-5 justify-center flex flex-wrap w-[100%] xl:w-[95%]">
+                <div className="h-[466.175px] w-[268px] bg-yellow-400">
+                    1
                 </div>
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px md:w-[277px] lg:w-[259px]  w-[268px] bg-blue-400">
-
+                <div className="h-[466.175px] w-[268px] bg-blue-400">
+                    2
                 </div>
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px md:w-[277px] lg:w-[259px]  w-[268px] bg-green-400">
-
+                <div className="h-[466.175px] w-[268px] bg-green-400">
+                    3
                 </div>
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px md:w-[277px] lg:w-[259px]  w-[268px] bg-amber-400">
-
+                <div className="h-[466.175px] w-[268px] bg-amber-400">
+                    4
                 </div>
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px md:w-[277px] lg:w-[259px]  w-[268px] bg-blue-400">
-
+                <div className="h-[466.175px] w-[268px] bg-blue-400">
+                    5
                 </div>
-                <div className="h-[466.175px] ty:w-[249px] sm:w-[281px md:w-[277px] lg:w-[259px]  w-[268px] bg-green-400">
-
+                <div className="h-[466.175px] w-[268px] bg-green-400">
+                    6
                 </div>
             </div>
         </div>
