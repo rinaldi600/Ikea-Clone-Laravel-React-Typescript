a5f989ad5811e5abb5e8829758f8761a2b6a34ed
rinaldi600 <rinaldih84@gmail.com>
2023-12-03 16:20:14 +0700

    Build product list 4

--- resources/js/Pages/Home/ProductList/ProductList.tsx
+++ resources/js/Pages/Home/ProductList/ProductList.tsx
@@ -1,8 +1,35 @@
+import React, { useRef, useState, useEffect } from "react";
+
 function ProductList() {
+
+    const parentRef = useRef<HTMLInputElement>(null)
+    const [number, setNumber] = useState<number>(0);
+
+    useEffect(() => {
+        console.log(number);
+        console.log(parentRef.current?.children[number]);
+    }, [number]);
+
+    const leftArrow = () => {
+        setNumber(number + 1);
+    }
+
+    const rightArrow = () => {
+        setNumber(number - 1);
+    }
+
     return (
-        <div className="pt-7">
-            <h1 className="text-3xl pb-5 font-bold">Produk Pilihan</h1>
-            <div className="min-h-[466.175px] xl:justify-around overflow-y-hidden xl:gap-5 justify-center flex flex-wrap w-[100%] xl:w-[95%]">
+        <div className="pt-7 relative">
+            <h1 className="text-3xl pb-5 font-bold xl:text-left text-center">Produk Pilihan</h1>
+
+            <div onClick={leftArrow} className="absolute inset-y-0 left-0 cursor-pointer flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[40px] h-[40px]">
+                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z" clipRule="evenodd" />
+                </svg>
+            </div>
+
+            <div ref={parentRef} className="h-[466.175px] xl:justify-around overflow-y-hidden xl:gap-5 justify-center flex flex-wrap w-[100%] gap-4 xl:w-[95%]">
+
                 <div className="h-[466.175px] w-[268px] bg-yellow-400">
                     1
                 </div>
@@ -21,6 +48,13 @@ function ProductList() {
                 <div className="h-[466.175px] w-[268px] bg-green-400">
                     6
                 </div>
+
+            </div>
+
+            <div onClick={rightArrow} className="absolute inset-y-0 right-0 cursor-pointer flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[40px] h-[40px]">
+                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clipRule="evenodd" />
+                </svg>
             </div>
         </div>
     );
