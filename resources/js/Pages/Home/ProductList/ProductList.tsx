f09b01c00a0dc344dd746838564cd861980a87f1
rinaldi600 <rinaldih84@gmail.com>
2024-01-04 23:00:08 +0700

    Build product list 9

--- resources/js/Pages/Home/ProductList/ProductList.tsx
+++ resources/js/Pages/Home/ProductList/ProductList.tsx
@@ -5,19 +5,19 @@ function ProductList() {
 
     const parentRef = useRef<HTMLInputElement>(null)
     const [number, setNumber] = useState<number>(0);
-    const { height, width } = useDimensions();
+    const { width } = useDimensions();
 
     useEffect(() => {
-        console.log(width);
-        console.log(height);
+        console.log(number);
+        console.log("Width : ", width)
     });
 
     const leftArrow = () => {
-        setNumber(( number - width ) / 2);
+        setNumber(( number - 400 ));
     }
 
     const rightArrow = () => {
-        setNumber(( number + width ) / 2);
+        setNumber((number + 400 ));
     }
 
     return (
@@ -29,7 +29,6 @@ function ProductList() {
                 <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z" clipRule="evenodd" />
                 </svg>
             </div>
-
             <div ref={parentRef} style={{'transform' : `translateX(${number}px)`}} className={`duration-300 h-[466.175px] xl:justify-around xl:gap-5 justify-center grid grid-flow-col w-[100%] gap-4 xl:w-[95%]`}>
                 <div className="h-[466.175px] w-[268px] bg-yellow-400">
                     1
