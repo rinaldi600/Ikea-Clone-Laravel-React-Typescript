ab8e0a8ff31404075fbe2a04c70b4f035600cc5c
rinaldi600 <rinaldih84@gmail.com>
2024-09-08 22:35:14 +0700

    add footer 3

---
+++ resources/js/Pages/Home/Footer/MainLinks/IkeaAppSectionOne/ButtonSectionOne.tsx
@@ -0,0 +1,16 @@
+import { Link } from "@inertiajs/react";
+
+function ButtonSectionOne(props : Readonly<{link : string, title : string}>) {
+    return (
+        <Link
+        className="w-[270px] h-[40px] text-[#111111] font-bold text-xs rounded-full border border-[#929292] hover:border-[#111111]"
+        href={props.link}
+        as="button"
+        type="button"
+    >
+        {props.title}
+    </Link>
+    )
+}
+
+export default ButtonSectionOne;
--- resources/js/Pages/Home/Footer/MainLinks/IkeaAppSectionOne/IkeaAppSectionOne.tsx
+++ resources/js/Pages/Home/Footer/MainLinks/IkeaAppSectionOne/IkeaAppSectionOne.tsx
@@ -1,13 +1,27 @@
 import ImagePhone from "../../../../../../img/home-footer-iphone.png";
+import ButtonSectionOne from "./ButtonSectionOne";
+
 
 function IkeaAppSectionOne() {
     return (
-        <div className="w-[50%] bg-yellow-200 flex">
+        <div className="w-[50%] bg-yellow-200 flex gap-8 items-center">
             <div className="max-w-[170px]">
                 <img src={ImagePhone} alt="Image Phone" />
             </div>
-            <div>
-                <h4>Aplikasi IKEA Indonesia</h4>
+            <div className="max-w-[318px]">
+                <h4 className="text-xl font-semibold text-[#111111] mb-[16px]">
+                    Aplikasi IKEA Indonesia
+                </h4>
+                <p className="text-sm text-[#484848] mb-[16px] leading-[1.3rem]">
+                    Melalui aplikasi IKEA Indonesia, Anda dapat bergabung
+                    menjadi IKEA Family dan mendapatkan banyak reward spesial.
+                    Download dan daftar sekarang. GRATIS!
+                </p>
+                <div>
+                    <ButtonSectionOne link="/" title="Download di iOS"/>
+                    <div className="mb-[16px]"></div>
+                    <ButtonSectionOne link="/" title="Download di Android"/>
+                </div>
             </div>
         </div>
     );
