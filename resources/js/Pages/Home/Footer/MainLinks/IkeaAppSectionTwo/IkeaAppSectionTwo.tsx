3fe9328f2fe5319a1de3db84d86df4ab065d6ab3
rinaldi600 <rinaldih84@gmail.com>
2024-09-29 20:28:35 +0700

    add footer 4

--- resources/js/Pages/Home/Footer/MainLinks/IkeaAppSectionTwo/IkeaAppSectionTwo.tsx
+++ resources/js/Pages/Home/Footer/MainLinks/IkeaAppSectionTwo/IkeaAppSectionTwo.tsx
@@ -1,11 +1,32 @@
-
-
 function IkeaAppSectionTwo() {
-    return (
-        <div className="w-[50%] bg-blue-400 flex">
 
+
+    return (
+        <div className="w-[50%] bg-blue-400 flex items-center">
+            <div>
+                <h5 className="font-semibold text-xl text-[#111111] mb-[16px]">
+                    Tautan Pengguna
+                </h5>
+                <ul className="flex flex-col flex-wrap max-h-[240px] text-sm text-[#111111] gap-2">
+                    <li>Kebijakan Pengembalian 90 Hari</li>
+                    <li>Layanan Pelanggan</li>
+                    <li>IKEA untuk Bisnis</li>
+                    <li>Hubungi Kami</li>
+                    <li>Pick-up Point IKEA</li>
+                    <li>Ini adalah IKEA</li>
+                    <li>Bekerja di IKEA</li>
+                    <li>FAQ</li>
+                    <li>Ruang berita</li>
+                    <li>Layanan IKEA</li>
+                    <li>Tinggalkan saran</li>
+                    <li>Diskon hingga 80%</li>
+                    <li>Harga Terjangkau</li>
+                    <li>Daftar kategori</li>
+                    <li>Sertifikasi produk</li>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
 
 export default IkeaAppSectionTwo;
--- resources/js/Pages/Home/Footer/MainLinks/MainLinks.tsx
+++ resources/js/Pages/Home/Footer/MainLinks/MainLinks.tsx
@@ -3,7 +3,7 @@ import IkeaAppSectionTwo from "./IkeaAppSectionTwo/IkeaAppSectionTwo";
 
 function MainLinks() {
     return (
-        <div className="min-h-[343.8px] w-[100%] flex bg-red-300 mx-auto flex-wrap">
+        <div className="min-h-[343.8px] w-[100%] flex mx-auto flex-wrap">
             <IkeaAppSectionOne/>
             <IkeaAppSectionTwo/>
         </div>
