c4ff7acfebf122a15bbbd7f87b7795801bb867e7
rinaldi600 <rinaldih84@gmail.com>
2025-01-05 20:50:28 +0700

    add login 3

--- resources/js/Pages/Login/Login.tsx
+++ resources/js/Pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import Layout from "../Layout/Layout";
 function Login() {
     return (
         <div className="min-h-screen w-full lg:pt-0 font-['ikea'] xl:pl-32">
-            <div className="min-h-[50px] pt-4 w-[100%] xl:w-[95%]">
+            <div className="min-h-[50px] pt-4 w-[90%] m-auto border-t xl:m-0 border-[#DEE2E6] xl:w-[95%]">
                 <div className="flex items-center gap-1 text-[#111111] text-xs">
                     <p>Client</p>
                     <p>
@@ -23,6 +23,14 @@ function Login() {
                         </svg>
                     </p>
                     <p>Masuk</p>
+                </div>
+                <div className="w-full bg-red-300 h-[300px] mt-2 flex">
+                    <div className="bg-green-300 w-full">
+                        <h1>Work</h1>
+                    </div>
+                    <div className="bg-green-500 w-full">
+                        <h1>WORK 2</h1>
+                    </div>
                 </div>
             </div>
         </div>
