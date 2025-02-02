3a5797334a7a1a7a9df90e36ca279e536bc29041
rinaldi600 <rinaldih84@gmail.com>
2025-02-02 23:13:34 +0700

    add login 4

--- resources/js/Pages/Login/Login.tsx
+++ resources/js/Pages/Login/Login.tsx
@@ -3,8 +3,8 @@ import Layout from "../Layout/Layout";
 function Login() {
     return (
         <div className="min-h-screen w-full lg:pt-0 font-['ikea'] xl:pl-32">
-            <div className="min-h-[50px] pt-4 w-[90%] m-auto border-t xl:m-0 border-[#DEE2E6] xl:w-[95%]">
-                <div className="flex items-center gap-1 text-[#111111] text-xs">
+            <div className="min-h-fit pt-4 w-[90%] m-auto border-t xl:m-0 border-[#DEE2E6] xl:w-[95%]">
+                <div className="flex items-center gap-1 text-[#111111] text-xs pb-[16px]">
                     <p>Client</p>
                     <p>
                         <svg
@@ -24,12 +24,33 @@ function Login() {
                     </p>
                     <p>Masuk</p>
                 </div>
-                <div className="w-full bg-red-300 h-[300px] mt-2 flex">
-                    <div className="bg-green-300 w-full">
-                        <h1>Work</h1>
+                <div className="w-full mt-2 flex pt-[16px]">
+                    <div className="w-full min-h-[781.688px] flex justify-center pt-[120px]">
+                        <h1 className="max-w-[390.633px] text-4xl font-semibold leading-[3.375rem]">
+                            Silahkan masuk ke akun IKEA Family Anda
+                        </h1>
                     </div>
-                    <div className="bg-green-500 w-full">
-                        <h1>WORK 2</h1>
+                    <div className="bg-green-500 w-full min-h-[781.688px]">
+                        <div className="max-w-[342px] bg-red-200 p-2">
+                            <div className="relative w-fit">
+                                <label
+                                    htmlFor="email"
+                                    className="text-[#484848] text-sm leading-5"
+                                >
+                                    Alamat email atau nomor handphone yang
+                                    terverifikasi
+                                </label>
+                                <span className="text-[#dc3545] absolute bottom-0 right-0">
+                                    *
+                                </span>
+                            </div>
+                            <input
+                                id="email"
+                                className="w-full rounded border-1 border-[#929292]"
+                                type="text"
+                                autoComplete="off"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
